c5c3a29d2a32567d169ecb452d4df0e053a9448f
Bartlomiej Frankow <bartlomiej.frankow@netguru.com>
2022-04-26 10:22:29 +0200

    DrawShapesScreen added

--- app/src/main/java/com/example/jetpackcomposecanvasexamples/presentation/DrawShapesScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jetpackcomposecanvasexamples.presentation
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DrawShapesScreen() {
-    Text(text = "Draw shapes")
-}
