096635d9a595d78561f096214e580299302be4a9
Bartlomiej Frankow <bartlomiej.frankow@netguru.com>
2022-04-27 15:51:17 +0200

    Add drag functionality and other texts

--- app/src/main/java/com/example/jetpackcomposecanvasexamples/presentation/main/drawerMenu/weightMeasurement/WeightMeasurementScreen.kt
+++ app/src/main/java/com/example/jetpackcomposecanvasexamples/presentation/main/drawerMenu/weightMeasurement/WeightMeasurementScreen.kt
@@ -1,39 +1,47 @@
 package com.example.jetpackcomposecanvasexamples.presentation.main.drawerMenu.weightMeasurement
 
-import android.graphics.Color
 import android.graphics.Paint
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.withRotation
+import com.example.jetpackcomposecanvasexamples.R
 import com.example.jetpackcomposecanvasexamples.presentation.main.drawerMenu.weightMeasurement.LineType.*
-import kotlin.math.PI
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.*
 
 private const val SHADOW_ALPHA = 50
 private const val SHADOW_RADIUS = 60f
 private const val NUMBER_SPACE = 5
 private const val NINETY_DEGREES_FLIP = 90
 private const val INDICATOR_SIDE_WIDTH = 4f
+private const val INITIAL_WEIGHT = 80
 
 @Composable
 fun WeightMeasurementScreen(
     modifier: Modifier = Modifier,
     style: ScaleStyle = ScaleStyle(),
     minWeight: Int = 0,
-    maxWeight: Int = 200,
-    initialWeight: Int = 80
+    maxWeight: Int = 120,
+    initialWeight: Int = INITIAL_WEIGHT
 ) {
     var center by remember {
         mutableStateOf(Offset.Zero)
@@ -44,12 +52,80 @@ fun WeightMeasurementScreen(
     var angle by remember {
         mutableStateOf(0f)
     }
+    var dragStateAngle by remember {
+        mutableStateOf(0f)
+    }
+    var oldAngle by remember {
+        mutableStateOf(angle)
+    }
+    var weight by remember {
+        mutableStateOf(INITIAL_WEIGHT)
+    }
 
     Box(modifier = Modifier.fillMaxSize()) {
+
+        Text(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 40.dp),
+            text = stringResource(R.string.select_your_weight),
+            fontSize = 32.sp,
+            fontFamily = FontFamily(Font(R.font.lexend_thin)),
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(color = Color.Black, fontSize = 48.sp)
+                ) {
+                    append(weight.toString())
+                }
+                withStyle(
+                    style = SpanStyle(color = Color.Green, fontSize = 16.sp)
+                ) {
+                    append(stringResource(R.string.kg))
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 120.dp),
+            fontFamily = FontFamily(Font(R.font.lexend_thin)),
+            fontWeight = FontWeight.Bold
+        )
+
         Canvas(
             modifier = modifier
                 .fillMaxWidth()
+                .height(200.dp)
                 .align(Alignment.Center)
+                .pointerInput(true) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            dragStateAngle = -atan2(
+                                y = circleCenter.x - offset.x,
+                                x = circleCenter.y - offset.y
+                            ).toDegrees()
+                        },
+                        onDragEnd = {
+                            oldAngle = angle
+                        }
+                    ) { change, _ ->
+                        val touchAngle = -atan2(
+                            y = circleCenter.x - change.position.x,
+                            x = circleCenter.y - change.position.y
+                        ).toDegrees()
+
+                        val newAngle = oldAngle + (touchAngle - dragStateAngle)
+
+                        angle = newAngle.coerceIn(
+                            minimumValue = initialWeight - maxWeight.toFloat(),
+                            maximumValue = initialWeight - minWeight.toFloat()
+                        )
+
+                        weight = (initialWeight - angle).roundToInt()
+                    }
+                }
         ) {
             center = this.center
             circleCenter = Offset(
@@ -162,7 +238,7 @@ private fun DrawScope.drawWeightValue(
                     Paint().apply {
                         textSize = style.textSize.toPx()
                         textAlign = Paint.Align.CENTER
-                    }
+                    },
                 )
             }
         }
@@ -177,9 +253,9 @@ private fun DrawScope.drawScale(circleCenter: Offset, style: ScaleStyle) {
             style.radius.toPx(),
             Paint().apply {
                 strokeWidth = style.scaleWidth.toPx()
-                color = Color.WHITE
+                color = Color.White.hashCode()
                 setStyle(Paint.Style.STROKE)
-                setShadowLayer(SHADOW_RADIUS, 0f, 0f, Color.argb(SHADOW_ALPHA, 0, 0, 0))
+                setShadowLayer(SHADOW_RADIUS, 0f, 0f, android.graphics.Color.argb(SHADOW_ALPHA, 0, 0, 0))
             }
         )
     }
